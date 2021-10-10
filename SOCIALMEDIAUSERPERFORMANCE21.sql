e88d5d7d4ecfbd97470e31724bac376e2527dadd
elizabethcmartinez <90217992+elizabethcmartinez@users.noreply.github.com>
2021-10-10 14:14:42 -0600

    Update SOCIALMEDIAUSERPERFORMANCE21.sql

--- SOCIALMEDIAUSERPERFORMANCE21.sql
+++ SOCIALMEDIAUSERPERFORMANCE21.sql
@@ -25,9 +25,10 @@ SUM([INSTAGRAM USERS (IN MILLION)]) OVER (PARTITION BY YEARS) AS TotalIgUsersPer
 from DBO.[SOCIAL MEDIA USERS]
 ORDER BY [PERCENT_TOTAL] DESC;
 
+ 
+-- AVG SOCIAL MEDIA USERS DURING 2020!!! FB: 2710.25 MILLION, TWITTER: 339.50 MILLION, 
+-- INSTAGRAM: 1033 MILLION or ONE BILLION 
 -- CTE 
--- AVG SOCIAL MEDIA USERS DURING 2020 FOR 
--- FB: 2710.25 MILLION, TWITTER: 339.50 MILLION, INSTAGRAM: 1033 MILLION or ONE BILLION 
 with CTE_SOCIALMEDIAUSERS AS (
 SELECT YEARS,
 AVG([FACEBOOK USERS (IN MILLION)]) AS AVG_FB_USERS_IN_MILLIONS, 
@@ -46,5 +47,37 @@ case when [INSTAGRAM USERS (IN MILLION)] is null then 0.00 else [INSTAGRAM USERS
 as [INSTAGRAM USERS (IN MILLION)]
 FROM DBO.[SOCIAL MEDIA USERS]
 
+-- Downward trend on user activity: as the number of followers increases, 
+-- the amount of like engagement decrease
+CREATE VIEW [ACTIVITY BASED ON FOLLOWING] AS 
+select followers, likes, likes * 100 / followers as 'Percentage(%)' 
+from instagram_reach$ 
+order by Followers ASC offset 0 rows
 
+select * from [ACTIVITY BASED ON FOLLOWING]
+	
+-- Engagement activity based on the Number of Usernames 
+select followers, likes, [time since posted], 
+count(username) over (partition by Username) 
+as TotalUsernames
+from instagram_reach$
+
+-- Percent of likes based on Hours Since Posted 
+-- HIGHEST 20-30% Range of likes based on AVG 3 Hours Past Post 
+select followers, likes, [Time since posted] as [Hours Since Posted], 
+convert(int, [Time since posted]) * 100 / Likes as 'percentage(%)'
+from instagram_reach$
+order by [percentage(%)] desc
+
+-- Average like engagement based on the average time since posted and # of followers
+select AVG([Time since posted]) as [Avg Time Since Posted], 
+AVG(followers) as AvgFollowers, 
+avg(Likes) as AverageLikes from instagram_reach$ 
+
+-- Self joining Followers and Like columns from same table
+select t1.Followers as Followers, 
+		t2.Likes as Likes
+from instagram_reach$ t1
+		inner join instagram_reach$ t2
+		on t1.followers = t2.Followers
 
